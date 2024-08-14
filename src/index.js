6eb154791305561f299376dd8785e67420e73342
dbz221 <157700441+dbz221@users.noreply.github.com>
2024-08-15 00:40:41 +0330

    improved user experience and updated the index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,16 @@
 function generate(event) {
   event.preventDefault();
-  //   let topic = document.querySelector("#topic");
+  let topic = document.querySelector("#topic");
   let apiKey = "ebet14afo803932798f53163dbb80c50";
-  let prompt = document.querySelector("#topic");
+  let prompt = `please generate the poem based on ${topic.value} and keep it short, clean and concise`;
+
   let context =
-    "You are a slam poet who composes short poems based on a given word, please generate the poem based on the given prompt and keep it short, clean and concise";
+    "You are a poet who composes short lyrical poems that rhyme based on a given word, make sure to present is in basic HTML written in separate lines using <br/>";
   let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+  let poemElement = document.querySelector("#poem");
+  poemElement.classList.remove("hidden");
+  poemElement.innerHTML = `<div class="generating">⏳ Generating a French poem about ${topic.value}</div>`;
+
   axios.get(apiURL).then(generatePoem);
 }
 
@@ -14,6 +19,7 @@ function generatePoem(response) {
     strings: response.data.answer,
     autoStart: true,
     delay: 50,
+    cursor: "",
   });
 }
 
