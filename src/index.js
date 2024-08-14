24806c3ca804ed7a5893fe86182391158308f982
dbz221 <157700441+dbz221@users.noreply.github.com>
2024-08-14 22:54:47 +0330

    Updated the api and related functions

--- src/index.js
+++ src/index.js
@@ -1,13 +1,22 @@
 function generate(event) {
   event.preventDefault();
   //   let topic = document.querySelector("#topic");
+  let apiKey = "ebet14afo803932798f53163dbb80c50";
+  let prompt = document.querySelector("#topic");
+  let context =
+    "You are a slam poet who composes short poems based on a given word, please generate the poem based on the given prompt and keep it short, clean and concise";
+  let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+  axios.get(apiURL).then(generatePoem);
+}
 
+function generatePoem(response) {
   new Typewriter("#poem", {
-    strings: "Road not taken",
+    strings: response.data.answer,
     autoStart: true,
     delay: 50,
   });
 }
 
 let generateBtn = document.querySelector("#generate-btn");
+
 generateBtn.addEventListener("click", generate);
